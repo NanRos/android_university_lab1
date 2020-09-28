dd681eef12479e8199919625b1b73027d1959a28
NanRos <nancyrosa956@gmail.com>
2020-09-27 20:51:21 -0700

    Connected Views and set onclicklistener

--- app/src/main/java/com/codepath/bestsellerlistapp/BestSellerBooksRecyclerViewAdapter.java
+++ app/src/main/java/com/codepath/bestsellerlistapp/BestSellerBooksRecyclerViewAdapter.java
@@ -1,12 +1,17 @@
 package com.codepath.bestsellerlistapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.codepath.bestsellerlistapp.models.BestSellerBook;
 
 import java.util.List;
@@ -33,10 +38,13 @@ public class BestSellerBooksRecyclerViewAdapter extends RecyclerView.Adapter<Bes
     }
 
     @Override
-    public void onBindViewHolder(final BookViewHolder holder, int position) {
+    public void onBindViewHolder(final BookViewHolder holder, final int position) {
         holder.mItem = books.get(position);
         holder.mBookTitle.setText(books.get(position).title);
         holder.mBookAuthor.setText(books.get(position).author);
+        holder.mBookRanking.setText(String.valueOf(books.get(position).rank));
+        Glide.with(holder.mView).load(books.get(position).bookImageUrl).centerInside().into(holder.mBookImage);
+        holder.mBookDiscript.setText(books.get(position).description);
 
         holder.mView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,6 +55,16 @@ public class BestSellerBooksRecyclerViewAdapter extends RecyclerView.Adapter<Bes
                     mListener.onItemClick(holder.mItem);
                 }
             }
+        });
+        holder.mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    String url = books.get(position).amazonUrl;
+                    Intent browserIntent = new Intent(Intent.ACTION_VIEW);
+                    browserIntent.setData(Uri.parse(url));
+                    holder.mView.getContext().startActivity(browserIntent);
+                }
+
         });
     }
 
@@ -59,6 +77,10 @@ public class BestSellerBooksRecyclerViewAdapter extends RecyclerView.Adapter<Bes
         public final View mView;
         public final TextView mBookTitle;
         public final TextView mBookAuthor;
+        public final TextView mBookRanking;
+        public final ImageView mBookImage;
+        public final TextView mBookDiscript;
+        public final Button mButton;
         public BestSellerBook mItem;
 
         public BookViewHolder(View view) {
@@ -66,6 +88,11 @@ public class BestSellerBooksRecyclerViewAdapter extends RecyclerView.Adapter<Bes
             mView = view;
             mBookTitle = (TextView) view.findViewById(R.id.book_title);
             mBookAuthor = (TextView) view.findViewById(R.id.book_author);
+            mBookRanking = (TextView)view.findViewById(R.id.ranking);
+            mBookImage = (ImageView)view.findViewById(R.id.book_image);
+            mBookDiscript = (TextView) view.findViewById(R.id.book_description);
+            mButton = (Button) view.findViewById(R.id.buy_button);
+            mButton.setText("BUY ON AMAZON");
         }
 
         @Override
